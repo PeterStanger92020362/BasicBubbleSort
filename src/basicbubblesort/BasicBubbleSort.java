9520b2fd02cf3691165de427aa538321b4f1af2d
Peter Stanger <92020362@learn.cpp.ac.nz>
2020-06-22 21:21:34 +1200

    updated counter code to be correct

--- src/basicbubblesort/BasicBubbleSort.java
+++ src/basicbubblesort/BasicBubbleSort.java
@@ -10,12 +10,59 @@ package basicbubblesort;
  * @author 92020362
  */
 public class BasicBubbleSort {
-
+    
+    
+    private static int parseCounter = 0;
+    private static int swapCheckCounter = 0;
     /**
      * @param args the command line arguments
      */
     public static void main(String[] args) {
-        // TODO code application logic here
+               
+        BasicBubbleSort ob = new BasicBubbleSort();
+        int arr[] = {64, 34, 25, 12, 22, 11, 90};
+        System.out.println("The Unsorted array is");
+        ob.printArray(arr);
+
+        // apply sort
+        ob.doBubbleSort(arr);
+        System.out.println("The Basic Bubble Sorted array is");
+        ob.printArray(arr);
+        System.out.println("The number of times the array was parsed was " + parseCounter);
+        System.out.println("The number of compare operations was " + swapCheckCounter);
+        
+    }
+    
+    void doBubbleSort(int arr[]) {
+        int n = arr.length;
+        
+        for (int i = 0; i < n - 1; i++) {
+            
+            
+            for (int j = 0; j < n - i - 1; j++) {
+                parseCounter ++;
+                
+                if (arr[j] > arr[j + 1]) {
+                    swapCheckCounter ++;
+                    // swap temp and arr[i]
+                    int temp = arr[j];
+                    arr[j] = arr[j + 1];
+                    arr[j + 1] = temp;
+                }
+            }
+            
+        }
+
+    }
+
+    /* Prints the array */
+    void printArray(int arr[]) {
+        int n = arr.length;
+        for (int i = 0; i < n; ++i) {
+            System.out.print(arr[i] + " ");
+        }
+        System.out.println();
+        System.out.println();
     }
     
 }
